3a3dd02e1027f3d825113b1f9eefbcbff61e42cd
thorstenreichelt <info@thorstenreichelt.de>
2015-10-20 18:29:29 +0200

    FritzBox Call History
    Get the Call History from your Fritzbox as a list with date ant time,
    telephone number of contact name, and some more options.
    
    You only need to change the IP of your fritzbox and the root password.

---
+++ fritzbox-calls.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#Version 1.0
+
+## Edit this ##
+FRITZBOX="IP_OF_FRITZBOX"
+PASSWORD="ROOT_PASSWORD_OF_FRITZBOX"
+
+
+## script ##
+
+challengeRsp=$(curl --header "Accept: application/xml" \
+    --header "Content-Type: text/plain"     \
+    "http://$FRITZBOX/login_sid.lua" 2>/dev/null)
+    
+challenge=$(echo $challengeRsp | sed "s/^.*<Challenge>//" | sed "s/<\/Challenge>.*$//")
+
+if [[ -z $challenge ]]; then
+    echo "No challenge found"
+    exit 0
+fi
+
+challenge_bf="$challenge-$PASSWORD"
+challenge_bf=$(echo -n $challenge_bf | iconv -f ISO8859-1 -t UTF-16LE | md5sum -b)
+challenge_bf=$(echo $challenge_bf | sed "s/ .*$//")
+response_bf="$challenge-$challenge_bf"
+
+url="http://$FRITZBOX/login_sid.lua"
+
+sidRsp=$(curl --header "Accept: text/html,application/xhtml+xml,application/xml" \
+    --header "Content-Type: application/x-www-form-urlencoded"      \
+    -d "response=$response_bf" \
+    $url 2>/dev/null)
+    
+sid=$(echo $sidRsp | sed "s/^.*<SID>//" | sed "s/<\/SID>.*$//")
+
+regex="^0+$"
+if [[ $sid =~ $regex ]]; then
+    echo "Invalid password"
+    exit 0
+fi
+
+IFS=' '
+stats=$(curl --header "Accept: application/xml" \
+    --header "Content-Type: text/plain"     \
+    "http://$FRITZBOX/fon_num/foncalls_list.lua?sid=$sid" 2>/dev/null)
+
+#Minimize Output 
+stats=$(echo $stats | sed ':a;N;$!ba;s/\n//g')
+stats=$(echo $stats | sed "s/.*Datum//" )
+stats=$(echo $stats | sed "s/btn_form.*$//" )
+
+# And output it
+echo $stats
